41256ba7f5783cd103c6994ca63858a70a9507f7
zjccc <1799513839@qq.com>
2023-08-15 22:27:31 +0800

    Initial commit

---
+++ src/test/java/cn/zjc/springframework/test/ApiTest.java
@@ -0,0 +1,4 @@
+package cn.zjc.springframework.test.bean.cn.zjc.springframework.test;
+
+public class ApiTest {
+}
---
+++ src/test/java/cn/zjc/springframework/test/bean/UserService.java
@@ -0,0 +1,5 @@
+package cn.zjc.springframework.test;
+
+public class UserService {
+
+        }
